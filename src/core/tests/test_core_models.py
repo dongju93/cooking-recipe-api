fac26e89de0e3eb71e427862ad75302aff5ee0f9
dongju93 <spdlqj011@gmail.com>
2026-02-19 01:31:52 +0900

    :label: Import right custom models

--- src/core/tests/test_core_models.py
+++ src/core/tests/test_core_models.py
@@ -1,12 +1,16 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+from ..models import User
+
 
 class ModelTests(TestCase):
-    def test_create_user_with_email_successful(self):
+    def test_create_user_with_email_successful(self) -> None:
         email: str = "test@example.com"
         password: str = "testpass123"
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user: User = get_user_model().objects.create_user(  # type: ignore[missing-attribute]
+            email=email, password=password
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
