b7366a52e0a8a062df56027d350211801f280cd3
dongju93 <spdlqj011@gmail.com>
2026-02-21 23:46:58 +0900

    :triangular_flag_on_post: User api endpoint

---
+++ src/user/urls.py
@@ -0,0 +1,18 @@
+"""URL configuration for the user API.
+
+`app_name = "user"` sets the application namespace for this URL module.
+Namespacing allows `reverse("user:create")` to resolve unambiguously even
+if another app registers a URL also named "create". This namespace is
+declared here and referenced via `include("user.urls")` in the root urls.py.
+"""
+
+from django.urls import path
+from django.urls.resolvers import URLPattern
+
+from .views import CreateUserView
+
+app_name = "user"
+
+urlpatterns: list[URLPattern] = [
+    path("create", CreateUserView.as_view(), name="create"),
+]
